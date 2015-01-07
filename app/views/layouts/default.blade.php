9e9ae4e306021899f87146463e198e13887b466c
Mark Everhart <mark@pigeonholellc.com>
2015-01-08 00:11:13 +0100

    Added Materialize CDN

---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="http://cdn.smtcs.rocks/md/md.css">
+</head>
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <script src="http://cdn.smtcs.rocks/md/md.js"></script>
+</body>
+</html>
